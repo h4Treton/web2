f587cf63b19b3fea8ef41774dd370ba9de9bdb77
h4Treton <h4Treton@gmail.com>
2024-02-22 10:09:39 +0300

    tt

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ import os
 from werkzeug.utils import secure_filename
 
 BASE_DIR = os.getcwd()
+
+app = Flask(__name__, static_folder=os.path.join(BASE_DIR, 'static'),
+            template_folder = os.path.join(BASE_DIR, 'templates'))
+
+app.config['SECRET_KEY'] = 'yuhasdfbb[PSDGOHpdofhgpoahghodfgh]'
+
+users = ['user1', 'user2', 'user3', 'user4']
+
+@app.route('/')
+def index():
+    res = ''
+    res += '<h4> USERS </h4>'
+    for user in users:
+        res += f"<p> пользователь {user} </p>"
+    return(res)
+
+
+app.run(debug=True)
